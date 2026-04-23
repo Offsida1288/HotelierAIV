27da9552f102ffdcedb22b66a2e0ed01ad62e81a
/HOT_Dev_Girl <badutsatu486@gmail.com>
2026-04-23 12:46:41 +0200

    Update main.sol

--- main.sol
+++ main.sol
@@ -68,3 +68,73 @@ library SafeERC20 {
 }
 
 library ECDSA {
+    error ECDSA_InvalidSig();
+    error ECDSA_InvalidS();
+    error ECDSA_InvalidV();
+
+    function recover(bytes32 digest, bytes memory signature) internal pure returns (address signer) {
+        if (signature.length != 65) revert ECDSA_InvalidSig();
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        if (uint256(s) > 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0) revert ECDSA_InvalidS();
+        if (v != 27 && v != 28) revert ECDSA_InvalidV();
+        signer = ecrecover(digest, v, r, s);
+        if (signer == address(0)) revert ECDSA_InvalidSig();
+    }
+}
+
+library MiniStrings {
+    error Str_BadHex();
+
+    function _fromHexChar(uint8 c) internal pure returns (uint8) {
+        if (c >= 48 && c <= 57) return c - 48;
+        if (c >= 65 && c <= 70) return c - 55;
+        if (c >= 97 && c <= 102) return c - 87;
+        revert Str_BadHex();
+    }
+
+    function parseHexAddress(string memory s) internal pure returns (address a) {
+        bytes memory b = bytes(s);
+        if (b.length != 42) revert Str_BadHex();
+        if (b[0] != "0" || (b[1] != "x" && b[1] != "X")) revert Str_BadHex();
+
+        uint160 out = 0;
+        for (uint256 i = 2; i < 42; i += 2) {
+            uint8 hi = _fromHexChar(uint8(b[i]));
+            uint8 lo = _fromHexChar(uint8(b[i + 1]));
+            out = (out << 8) | uint160((hi << 4) | lo);
+        }
+        a = address(out);
+    }
+}
+
+abstract contract ReentrancyGuard {
+    uint256 private _gate;
+    modifier nonReentrant() {
+        if (_gate == 2) revert();
+        _gate = 2;
+        _;
+        _gate = 1;
+    }
+
+    constructor() {
+        _gate = 1;
+    }
+}
+
+abstract contract Pausable {
+    event PauseFlip(address indexed operator, bool isPaused, uint256 at);
+    bool private _paused;
+
+    modifier whenNotPaused() {
+        if (_paused) revert();
+        _;
+    }
